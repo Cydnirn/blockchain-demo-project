8592d0a44d4174db96dc761f73746c21599060d4
Seno Aji <cydnirn.dev@gmail.com>
2025-07-28 16:38:09 +0700

    refactor: simplify key generation and wallet creation process

--- Blockchain-Demo-Project/Wallet.cs
+++ Blockchain-Demo-Project/Wallet.cs
@@ -1,17 +1,20 @@
-namespace Blockchain_Demo_Project;
 using System.Security.Cryptography;
+using Blockchain_Demo_Project.Interfaces;
 
-public interface IKeyGenerator
+namespace Blockchain_Demo_Project;
+
+internal abstract class KeyGenerator
 {
-    (byte[] publicKey, byte[] privateKey) GenerateKeyPair();
+    public abstract (byte[] publicKey, byte[] privateKey) GenerateKeyPair();
+    public abstract byte[] ExportKeyPair(string privateKey);
 }
 
 /// Generates cryptographic key pairs for wallets using the secp256k1 curve
-public class KeyGenerator : IKeyGenerator
+internal class EcdsaKeyGenerator : KeyGenerator
 {
     private const string CurveName = "secp256k1";
 
-    public (byte[] publicKey, byte[] privateKey) GenerateKeyPair()
+    public override (byte[] publicKey, byte[] privateKey) GenerateKeyPair()
     {
         using var ecdsa = ECDsa.Create(ECCurve.CreateFromFriendlyName(CurveName));
         var privateKey = ecdsa.ExportECPrivateKey();
@@ -19,13 +22,23 @@ public class KeyGenerator : IKeyGenerator
 
         return (publicKey, privateKey);
     }
+
+    /// Exports the public key from a given private key in hexadecimal format.
+    /// The private key is expected to be in hexadecimal string format.
+    /// Returns the public key in SubjectPublicKeyInfo format.
+    public override byte[] ExportKeyPair(string privateKey)
+    {
+        using var ecdsa = ECDsa.Create(ECCurve.CreateFromFriendlyName(CurveName));
+        ecdsa.ImportECPrivateKey(Convert.FromHexString(privateKey), out _);
+        return ecdsa.ExportSubjectPublicKeyInfo();
+    }
 }
 
 /// Represents a wallet in the blockchain system.
 /// Contains a public key and a private key for cryptographic operations.
 /// The public key is used to identify the wallet, while the private key is used for signing
 /// transactions and proving ownership of the wallet's funds.
-public class Wallet : IWallet
+public class Wallet
 {
     public string PublicKey { get; }
     public string PrivateKey { get; }
@@ -36,27 +49,48 @@ public class Wallet : IWallet
         PrivateKey = privateKey ?? throw new ArgumentNullException(nameof(privateKey));
     }
 
-    public static IWallet Create()
+    internal static Wallet Create()
     {
-        var keyGenerator = new KeyGenerator();
+        var keyGenerator = new EcdsaKeyGenerator();
         var factory = new WalletFactory(keyGenerator);
         return factory.CreateWallet();
+    }
+
+    internal static Wallet Export(string privateKey)
+    {
+        var keyGenerator = new EcdsaKeyGenerator();
+        var factory = new WalletFactory(keyGenerator);
+        return factory.ExportWallet(privateKey);
     }
 }
 
 /// Factory class responsible for creating wallet instances
-public class WalletFactory (IKeyGenerator _keyGenerator)
+internal class WalletFactory (KeyGenerator keyGenerator)
 {
-    public IWallet CreateWallet()
+    public Wallet CreateWallet()
     {
         try
         {
-            var (publicKey, privateKey) = _keyGenerator.GenerateKeyPair();
+            var (publicKey, privateKey) = keyGenerator.GenerateKeyPair();
             return new Wallet(Convert.ToHexString(publicKey), Convert.ToHexString(privateKey));
         }
         catch (Exception e)
         {
             Console.WriteLine("Error generating wallet: " + e.Message);
+            throw;
+        }
+    }
+
+    public Wallet ExportWallet(string privateKey)
+    {
+        try
+        {
+            var publicKey = keyGenerator.ExportKeyPair(privateKey);
+            return new Wallet(Convert.ToHexString(publicKey), privateKey);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Error exporting wallet: " + e.Message);
             throw;
         }
     }
