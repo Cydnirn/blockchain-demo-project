d9fcc77e3d5d60ebf5942c3348b5d886fe251666
Seno Aji <cydnirn.dev@gmail.com>
2025-08-01 01:10:08 +0700

    refactor: simplify Block class by removing abstract base class and improving property accessors

--- Blockchain-Demo-Project/Blockchain/Block.cs
+++ Blockchain-Demo-Project/Blockchain/Block.cs
@@ -3,26 +3,16 @@ using Blockchain_Demo_Project.Interfaces;
 
 namespace Blockchain_Demo_Project.Blockchain;
 
-public abstract class BlockBase : IBlock
+public class Block(string previousHash, IReadOnlyList<ITransact> transactions) : IBlock
 {
-    public abstract string PreviousHash { get; }
-    public abstract string Hash { get; protected set; }
-    public abstract IReadOnlyList<ITransact> TransactionsReadOnly { get; }
-    public abstract int Nonce { get; protected set; }
-    public abstract void GenerateHash();
-    public abstract bool ValidBlock(int difficulty = 2);
-}
-
-public class Block(string previousHash, IReadOnlyList<ITransact> transactions) : BlockBase
-{
-    public override string PreviousHash { get; } = previousHash;
-    public override string Hash { get; protected set; } = "0"; // Default hash value, will be updated after mining
+    public string PreviousHash { get; } = previousHash;
+    public string Hash { get; private set; } = "0"; // Default hash value, will be updated after mining
     private string Timestamp { get; } = DateTimeOffset.UtcNow.ToUnixTimeSeconds().ToString();
     private List<ITransact> Transactions { get; } = [..transactions];
-    public override IReadOnlyList<ITransact> TransactionsReadOnly { get; } = [..transactions];
-    public override int Nonce { get; protected set; }
+    public IReadOnlyList<ITransact> TransactionsReadOnly { get; } = [..transactions];
+    public int Nonce { get; private set; }
     public void IncrementNonce() => Nonce++;
-    public override void GenerateHash()
+    public void GenerateHash()
     {
         // Concatenate the previous hash, timestamp, nonce, and transaction signatures to create a unique hash for the block
         Hash = Convert.ToHexString(SHA256.HashData(
@@ -35,7 +25,7 @@ public class Block(string previousHash, IReadOnlyList<ITransact> transactions) :
         );
     }
 
-    public override bool ValidBlock(int difficulty = 2)
+    public bool ValidBlock(int difficulty = 2)
     {
         // Check if the block's hash starts with the required number of leading zeros based on the difficulty
         var validHash = Hash.StartsWith(new string('0', difficulty));
